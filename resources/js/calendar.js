ce6afdd033fd9b218addd2ac93b88f6b8f86f716
Chanan E <chanan@slri.or.th>
2021-09-17 09:50:18 +0700

    calendar (js) added to instruments

---
+++ resources/js/calendar.js
@@ -0,0 +1,75 @@
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const today = new Date();
+console.log(today);
+const currentMonth = today.getMonth();
+const currentYear = today.getFullYear();
+var month=currentMonth;
+var year=currentYear;
+console.log(year);
+
+function UpdateCalendar(){
+    month=10;
+    year=2021;
+    ModCell(month,year);
+}
+
+function DisplayNextMonth(){
+    if (month < 11){
+        month++;
+    } else if (month == 11) {
+        month = 0;
+        year++;
+    }
+    ModCell(month,year);
+}
+
+function DisplayPreviousMonth(){
+    if (month > 0){
+        month--;
+    } else if (month == 0) {
+        month = 11;
+        year--;
+    }
+    ModCell(month,year);
+}
+
+function DisplayCurrentMonth(){
+    month=currentMonth;
+    year=currentYear;
+    ModCell(month,year);
+}
+
+function ModCell(month,year) {
+    //Borrow algorithms from https://medium.com/@nitinpatel_20236/challenge-of-building-a-calendar-with-pure-javascript-a86f1303267d
+    
+    let firstDay = new Date(year, month).getDay();
+    let nextMonth = new Date(year, month, 32).getDate();
+    let daysInMonth = 32-nextMonth;
+
+    let cellName;
+    let cell;
+    let calendarDate=(-1*firstDay)+1;
+    let contentString;
+
+    for (let i=1;i<=6;i++){
+        for (let j=0;j<=6;j++){
+            cellName=i+"_"+j;
+            if (calendarDate > 0 && calendarDate <= daysInMonth){
+                //document.getElementById(cellName).innerHTML='<p>'+calendarDate+'</p>';
+                contentString = "<table><tr>"
+                contentString += "<td id='"+i+"_"+j+"_1' style='color:#cccccc;font-size:8px'>●</td>"
+                contentString += "<td id='"+i+"_"+j+"_2' style='color:#cccccc;font-size:8px'>●</td>"
+                contentString += "<td id='"+i+"_"+j+"_3' style='color:#cccccc;font-size:8px'>●</td></tr>"
+                contentString += "<tr><td></table>"+calendarDate;
+                
+                document.getElementById(cellName).innerHTML = contentString;
+                  
+            } else {
+                document.getElementById(cellName).innerHTML="";
+            }
+            calendarDate++;
+        }
+    }
+    let calendarTitle = document.getElementById("calendar_title");
+    calendarTitle.innerHTML = months[month] + " " + year; 
+}
--- resources/views/instruments/show.blade.php
+++ resources/views/instruments/show.blade.php
@@ -42,11 +42,89 @@
                         </tr>
                     </table>
                     
+                    <div class="container col-sm-6 col-md-6 col-lg-6 mt-10 float-left">
+                        <h3>Instrument calendar</h3>
+                        <x-button class='button' onclick='DisplayPreviousMonth()'>Previous</x-button> 
+                        <x-button style='background-color:#5588EE' class='button' onclick='DisplayCurrentMonth()'>Current</x-button>
+                        <x-button class='button' onclick='DisplayNextMonth()'>Next</x-button>
+                        <div class="card">
+                            <h3 id='calendar_title' ></h3>
+                            <table name='calendar'>
+                                <tr>
+                                    <th>Sun</th>
+                                    <th>Mon</th>
+                                    <th>Tue</th>
+                                    <th>Wed</th>
+                                    <th>Thu</th>
+                                    <th>Fri</th>
+                                    <th>Sat</th>
+                                </tr>
+                                <tr>
+                                    <td id='1_0'></td>
+                                    <td id='1_1'></td>
+                                    <td id='1_2'></td>
+                                    <td id='1_3'></td>
+                                    <td id='1_4'></td>
+                                    <td id='1_5'></td>
+                                    <td id='1_6'></td>
+                                </tr>
+                                <tr>
+                                    <td id='2_0'></td>
+                                    <td id='2_1'></td>
+                                    <td id='2_2'></td>
+                                    <td id='2_3'></td>
+                                    <td id='2_4'></td>
+                                    <td id='2_5'></td>
+                                    <td id='2_6'></td>
+                                </tr>
+                                <tr>
+                                    <td id='3_0'></td>
+                                    <td id='3_1'></td>
+                                    <td id='3_2'></td>
+                                    <td id='3_3'></td>
+                                    <td id='3_4'></td>
+                                    <td id='3_5'></td>
+                                    <td id='3_6'></td>
+                                </tr>
+                                <tr>
+                                    <td id='4_0'></td>
+                                    <td id='4_1'></td>
+                                    <td id='4_2'></td>
+                                    <td id='4_3'></td>
+                                    <td id='4_4'></td>
+                                    <td id='4_5'></td>
+                                    <td id='4_6'></td>
+                                </tr>
+                                <tr>
+                                    <td id='5_0'></td>
+                                    <td id='5_1'></td>
+                                    <td id='5_2'></td>
+                                    <td id='5_3'></td>
+                                    <td id='5_4'></td>
+                                    <td id='5_5'></td>
+                                    <td id='5_6'></td>
+                                </tr>
+                                <tr>
+                                    <td id='6_0'></td>
+                                    <td id='6_1'></td>
+                                    <td id='6_2'></td>
+                                    <td id='6_3'></td>
+                                    <td id='6_4'></td>
+                                    <td id='6_5'></td>
+                                    <td id='6_6'></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <br>
+                    <a href="{{ route('instruments.edit',$instrumentDetail -> id) }}">Edit instrument detail</a>
+                    <br>
+                    <a href="{{ route('instruments.index') }}">Return to instrument list</a>
                 </div>
-                <a href="{{ route('instruments.edit',$instrumentDetail -> id) }}">Edit instrument detail</a>
-                <br>
-                <a href="{{ route('instruments.index') }}">Return to instrument list</a>
-                <br>
+                
+            </div>
         </div>
     </div>
+    <script type="text/javascript" src="{{ URL::asset('js/calendar.js') }}"></script>
+    <script>DisplayCurrentMonth();</script>
 </x-app-layout>
